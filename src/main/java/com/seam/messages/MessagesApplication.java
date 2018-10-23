df5a5519d2e47e88838311638d86d3839439eab6
Sanjay <dev.sanjaydas@gmail.com>
2018-10-23 12:36:32 +0530

    Corrected package name in component scan

--- src/main/java/com/seam/messages/MessagesApplication.java
+++ src/main/java/com/seam/messages/MessagesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@ComponentScan({"com.seam.messages","com.seam.pojo","com.seam.utils","com.seam.services"})
+@ComponentScan({"com.seam.messages","com.seam.services"})
 @EnableSwagger2
 public class MessagesApplication {
 
